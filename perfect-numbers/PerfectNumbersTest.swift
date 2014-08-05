bcc1f266f057d178a325118754f62451f92e71d8
Andrew Halls <andrew@galtsoft.com>
2014-08-05 09:37:12 +0800

    Added Nicomachus classification problem

---
+++ perfect-numbers/PerfectNumbersTest.swift
@@ -0,0 +1,41 @@
+
+import Foundation
+import XCTest
+
+class PerfectNumbersTest: XCTestCase {
+
+
+    func testPerfect() {
+        let numberClassifier = NumberClassifier(number: 6)
+        let expectedValue = NumberClassification.Perfect
+        let result = numberClassifier.classification
+        XCTAssertEqual(result,expectedValue);
+
+    }
+    
+    func testPerfectAgain() {
+        let numberClassifier = NumberClassifier(number: 28)
+        let expectedValue = NumberClassification.Perfect
+        let result = numberClassifier.classification
+        XCTAssertEqual(result,expectedValue);
+        
+    }
+
+    func testDeficient() {
+        let numberClassifier = NumberClassifier(number: 13)
+        let expectedValue = NumberClassification.Deficient
+        let result = numberClassifier.classification
+        XCTAssertEqual(result,expectedValue);
+        
+    }
+    
+    func testAbundent() {
+        let numberClassifier = NumberClassifier(number: 12)
+        let expectedValue = NumberClassification.Abundent
+        let result = numberClassifier.classification
+        XCTAssertEqual(result,expectedValue);
+        
+    }
+
+
+}
