0989f62ff6c5179d03da219195c32a21f52f3783
Hank Turowski <hank.turowski@gmail.com>
2014-10-05 11:13:42 -0400

    Updating for Swift 1.0

--- allergies/AllergiesTest.swift
+++ allergies/AllergiesTest.swift
@@ -2,45 +2,45 @@ import Foundation
 import XCTest
 
 class AllergiesTest: XCTestCase {
-
-  func test_bob() {
-
-    let allergies = Allergies(34)
-
-    XCTAssertTrue(allergies & Allergies.peanuts, "Bob is allergic to peanuts")
-    XCTAssertTrue(allergies & Allergies.chocolate, "Bob is allergic to chocolate")
-    XCTAssertFalse(allergies & Allergies.cats,  "Bob is not allergic to cats")
-  }
-
-  func test_eggsNcats () {
-
-    let allergies = Allergies(129)
-
-    XCTAssertTrue(allergies & Allergies.eggs)
-    XCTAssertTrue(allergies & Allergies.cats)
-    XCTAssertFalse(allergies & Allergies.chocolate)
-
-  }
-
-  func test_none() {
-    let allergies = Allergies(0)
-
-    XCTAssertFalse(allergies & Allergies.pollen)
-  }
-
-  func testAll() {
-
-    let allInt =  UInt(Array(0...7).reduce(0){ return ($0 | (1 << $1)) })
-    let allergies = Allergies(allInt)
-
-    XCTAssertTrue(allergies & Allergies.eggs)
-    XCTAssertTrue(allergies & Allergies.peanuts)
-    XCTAssertTrue(allergies & Allergies.shellfish)
-    XCTAssertTrue(allergies & Allergies.strawberries)
-    XCTAssertTrue(allergies & Allergies.tomatoes)
-    XCTAssertTrue(allergies & Allergies.chocolate)
-    XCTAssertTrue(allergies & Allergies.pollen)
-    XCTAssertTrue(allergies & Allergies.cats)
-
-  }
+    
+    func test_bob() {
+        
+        let allergies = Allergies(34)
+        
+        XCTAssertTrue(allergies.hasAllergy(Allergies.peanuts), "Bob is allergic to peanuts")
+        XCTAssertTrue(allergies.hasAllergy(Allergies.chocolate), "Bob is allergic to chocolate")
+        XCTAssertFalse(allergies.hasAllergy(Allergies.cats),  "Bob is not allergic to cats")
+    }
+    
+      func test_eggsNcats () {
+    
+        let allergies = Allergies(129)
+    
+        XCTAssertTrue(allergies.hasAllergy(Allergies.eggs))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.cats))
+        XCTAssertFalse(allergies.hasAllergy(Allergies.chocolate))
+    
+      }
+    
+      func test_none() {
+        let allergies = Allergies(0)
+    
+        XCTAssertFalse(allergies.hasAllergy(Allergies.pollen))
+      }
+    
+      func testAll() {
+    
+        let allInt =  UInt(Array(0...7).reduce(0){ return ($0 | (1 << $1)) })
+        let allergies = Allergies(allInt)
+    
+        XCTAssertTrue(allergies.hasAllergy(Allergies.eggs))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.peanuts))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.shellfish))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.strawberries))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.tomatoes))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.chocolate))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.pollen))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.cats))
+    
+      }
 }
--- allergies/example.swift
+++ allergies/example.swift
@@ -1,22 +1,26 @@
 import Foundation
 
-struct Allergies : RawOptionSetType {
-  private var value: UInt = 0
-  init(_ value: UInt) { self.value = value }
-  var boolValue: Bool { return self.value != 0 }
-  func toRaw() -> UInt { return self.value }
-  static func fromRaw(raw: UInt) -> Allergies? { return self(raw) }
-  static func fromMask(raw: UInt) -> Allergies { return self(raw) }
-  static func convertFromNilLiteral() -> Allergies { return self(0) }
-
-  static var None: Allergies { return self(0) }
-  static var eggs: Allergies { return Allergies(1 << 0) }
-  static var peanuts: Allergies { return Allergies(1 << 1) }
-  static var shellfish: Allergies { return Allergies(1 << 2) }
-  static var strawberries: Allergies { return Allergies(1 << 3) }
-  static var tomatoes: Allergies { return Allergies(1 << 4) }
-  static var chocolate: Allergies { return Allergies(1 << 5) }
-  static var pollen: Allergies { return Allergies(1 << 6) }
-  static var cats: Allergies { return Allergies(1 << 7) }
+struct Allergies {
+    private var value: UInt = 0
+    init(_ value: UInt) { self.value = value }
+    var boolValue: Bool { return self.value != 0 }
+    func toRaw() -> UInt { return self.value }
+    static func fromRaw(raw: UInt) -> Allergies? { return self(raw) }
+    static func fromMask(raw: UInt) -> Allergies { return self(raw) }
+    static func convertFromNilLiteral() -> Allergies { return self(0) }
+    
+    func hasAllergy(testAgainst: Allergies) -> Bool {
+        return self.value & testAgainst.value  > 0
+    }
+    
+    static var None: Allergies { return self(0) }
+    static var eggs: Allergies { return Allergies(1 << 0) }
+    static var peanuts: Allergies { return Allergies(1 << 1) }
+    static var shellfish: Allergies { return Allergies(1 << 2) }
+    static var strawberries: Allergies { return Allergies(1 << 3) }
+    static var tomatoes: Allergies { return Allergies(1 << 4) }
+    static var chocolate: Allergies { return Allergies(1 << 5) }
+    static var pollen: Allergies { return Allergies(1 << 6) }
+    static var cats: Allergies { return Allergies(1 << 7) }
 }
 func == (lhs: Allergies, rhs: Allergies) -> Bool { return lhs.value == rhs.value }
