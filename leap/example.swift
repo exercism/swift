d56ab29e4e601880ffe0a1d65b809d9ec767b38a
Hank Turowski <hank.turowski@gmail.com>
2014-10-05 11:19:05 -0400

    Updating leap for Swift 1.0

--- leap/example.swift
+++ leap/example.swift
@@ -1,11 +1,3 @@
-//
-//  Leap.swift
-//  ExercismConversion
-//
-//  Created by Hank Turowski on 6/11/14.
-//  Copyright (c) 2014 Turowski. All rights reserved.
-//
-
 import Foundation
 
 class Year {
@@ -15,7 +7,7 @@ class Year {
     }
     var isLeapYear: Bool {
     get {
-        return (year%4 == 0 && year%100 != 0 ) || year%400 == 0;
+        return (year%4 == 0 && year%100 != 0 ) || year%400 == 0
     }
     }
 }
