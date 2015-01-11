063e01377112af13b383c434c32bc215dcf50af1
Masters3d <cheyo@masters3d.com>
2015-01-11 10:50:42 -0800

    Added SumOfMultiples Swift

---
+++ sum-of-multiples/example.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func SumOfMultiples(limit:Int,  _  inMultiples:[Int] = [] ) -> Int{
+    var multiples = inMultiples
+    var arrayLimit = Array(1..<limit)
+    
+    if multiples.count == 0 {
+        multiples = [3, 5]
+    } else if multiples[0] == 0{
+        multiples = Array(dropFirst(inMultiples))
+    }
+    
+    var itemToReturn = 0
+    
+    for each in arrayLimit{
+        var lastAdded = 0
+        for multiple in multiples {
+            if each % multiple == 0 {
+                if lastAdded != each {
+                    itemToReturn += each
+                    lastAdded = each
+                }
+            }
+        }
+    }
+    return itemToReturn
+}
