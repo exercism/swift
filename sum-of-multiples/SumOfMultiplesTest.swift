530bd0a1f3d4c1324800414b986d3343cc01ef3b
Masters3d <cheyo@masters3d.com>
2015-01-13 16:50:26 -0800

    updated Sum of Multiples to match Ruby

--- sum-of-multiples/SumOfMultiplesTest.swift
+++ sum-of-multiples/SumOfMultiplesTest.swift
@@ -1,46 +1,46 @@
 import XCTest
 
 
-class SumOfMultiplesTest: XCTestCase {
+class SumOfMultiplesTest:XCTestCase {
     
     
     func testSumTo1(){
-        let result = SumOfMultiples(1)
+        let result = SumOfMultiples.toLimit(1)
         
         XCTAssertTrue(0 == result)
     }
     
     func testSumTo3(){
-        XCTAssertTrue(3 == SumOfMultiples(4))
+        XCTAssertTrue(3 == SumOfMultiples.toLimit(4))
     }
     
     func testSumTo10(){
-        XCTAssertTrue(23 == SumOfMultiples(10))
+        XCTAssertTrue(23 == SumOfMultiples.toLimit(10))
     }
    
     func testSumTo1000(){
-        XCTAssertTrue(233168 == SumOfMultiples(1000))
+        XCTAssertTrue(233168 == SumOfMultiples.toLimit(1000))
     }
     
     func testConfigurable_7_13_17_to_20(){
-        XCTAssertTrue(51 == SumOfMultiples(20, [7, 13, 17]))
+        XCTAssertTrue(51 == SumOfMultiples.toLimit(20, inMultiples: [7, 13, 17]))
 
     }
     
     func testConfigurable_43_47_to_10000(){
-        XCTAssertTrue(2203160 == SumOfMultiples(10000, [43, 47]))
+        XCTAssertTrue(2203160 == SumOfMultiples.toLimit(10000, inMultiples: [43, 47]))
         
     }
 
     
     func testConfigurable_0_to_10(){
-        XCTAssertTrue(0 == SumOfMultiples(10, [0]))
+        XCTAssertTrue(0 == SumOfMultiples.toLimit(10, inMultiples: [0]))
         
     }
 
 
     func testConfigurable_0_1_to_10(){
-        XCTAssertTrue(45 == SumOfMultiples(10, [0, 1]))
+        XCTAssertTrue(45 == SumOfMultiples.toLimit(10, inMultiples: [0, 1]))
     }
     
 }
