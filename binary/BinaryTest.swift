9f72f443d64f6eae9e7e42febcabf3803ea230ac
Masters3d <cheyo@masters3d.com>
2015-01-24 23:54:16 -0800

    Updated Asserts to Equal instead of Truth

--- binary/BinaryTest.swift
+++ binary/BinaryTest.swift
@@ -3,28 +3,28 @@ import XCTest
 class BinaryTests: XCTestCase {
     
     func test_binary_1_is_decimal_1(){
-        XCTAssertTrue( 1 == Binary("1").toDecimal)}
+        XCTAssertEqual( 1, Binary("1").toDecimal)}
     
     func test_binary_10_is_decimal_2(){
-        XCTAssertTrue( 2 == Binary("10").toDecimal)}
+        XCTAssertEqual( 2, Binary("10").toDecimal)}
     
     func test_binary_11_is_decimal_3(){
-        XCTAssertTrue( 3 == Binary("11").toDecimal)}
+        XCTAssertEqual( 3, Binary("11").toDecimal)}
     
     func test_binary_100_is_decimal_4(){
-        XCTAssertTrue( 4 == Binary("100").toDecimal)}
+        XCTAssertEqual( 4, Binary("100").toDecimal)}
     
     func test_binary_1001_is_decimal_9(){
-        XCTAssertTrue( 9 == Binary("1001").toDecimal)}
+        XCTAssertEqual( 9, Binary("1001").toDecimal)}
     
     func test_binary_11010_is_decimal_26(){
-        XCTAssertTrue( 26 == Binary("11010").toDecimal)}
+        XCTAssertEqual( 26, Binary("11010").toDecimal)}
     
     func test_binary_10001101000_is_decimal_1128(){
-        XCTAssertTrue( 1128 == Binary("10001101000").toDecimal)}
+        XCTAssertEqual( 1128, Binary("10001101000").toDecimal)}
     
     func test_invalid_binary_is_decimal_0(){
-        XCTAssertTrue( 0 == Binary("carrot123").toDecimal)}
+        XCTAssertEqual( 0, Binary("carrot123").toDecimal)}
     
     
 }
--- roman-numerals/RomanNumeralsTest.swift
+++ roman-numerals/RomanNumeralsTest.swift
@@ -3,57 +3,57 @@ import UIKit
 
 class  RomanNumeralsTest: XCTestCase {
     func test_1(){
-        XCTAssertTrue("I" == 1.toRoman)}
+        XCTAssertEqual("I", 1.toRoman)}
     
     func test_2(){
-        XCTAssertTrue("II" == 2.toRoman)}
+        XCTAssertEqual("II", 2.toRoman)}
     
     func test_3(){
-        XCTAssertTrue("III" == 3.toRoman)}
+        XCTAssertEqual("III", 3.toRoman)}
     
     func test_4(){
-        XCTAssertTrue("IV" == 4.toRoman)}
+        XCTAssertEqual("IV", 4.toRoman)}
     
     func test_5(){
-        XCTAssertTrue("V" == 5.toRoman)}
+        XCTAssertEqual("V", 5.toRoman)}
     
     func test_6(){
-        XCTAssertTrue("VI" == 6.toRoman)}
+        XCTAssertEqual("VI", 6.toRoman)}
     
     func test_9(){
-        XCTAssertTrue("IX" == 9.toRoman)}
+        XCTAssertEqual("IX", 9.toRoman)}
     
     func test_27(){
-        XCTAssertTrue("XXVII" == 27.toRoman)}
+        XCTAssertEqual("XXVII", 27.toRoman)}
     
     func test_48(){
-        XCTAssertTrue("XLVIII" == 48.toRoman)}
+        XCTAssertEqual("XLVIII", 48.toRoman)}
     
     func test_59(){
-        XCTAssertTrue("LIX" == 59.toRoman)}
+        XCTAssertEqual("LIX", 59.toRoman)}
     
     func test_93(){
-        XCTAssertTrue("XCIII" == 93.toRoman)}
+        XCTAssertEqual("XCIII", 93.toRoman)}
     
     func test_141(){
-        XCTAssertTrue("CXLI" == 141.toRoman)}
+        XCTAssertEqual("CXLI", 141.toRoman)}
     
     func test_163(){
-        XCTAssertTrue("CLXIII" == 163.toRoman)}
+        XCTAssertEqual("CLXIII", 163.toRoman)}
     
     func test_402(){
-        XCTAssertTrue("CDII" == 402.toRoman)}
+        XCTAssertEqual("CDII", 402.toRoman)}
     
     func test_575(){
-        XCTAssertTrue("DLXXV" == 575.toRoman)}
+        XCTAssertEqual("DLXXV", 575.toRoman)}
     
     func test_911(){
-        XCTAssertTrue("CMXI" == 911.toRoman)}
+        XCTAssertEqual("CMXI", 911.toRoman)}
     
     func test_1024(){
-        XCTAssertTrue("MXXIV" == 1024.toRoman)}
+        XCTAssertEqual("MXXIV", 1024.toRoman)}
     
     func test_3000(){
-        XCTAssertTrue("MMM" == 3000.toRoman)}
+        XCTAssertEqual("MMM", 3000.toRoman)}
     
 }
