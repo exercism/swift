927456e9e23a9dc18313a40e15502a52262db09b
Masters3d <cheyo@masters3d.com>
2015-02-23 17:50:36 -0800

    luhn

---
+++ luhn/example.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+
+struct Luhn {
+
+    var number:Double = 0
+    var addends:[Int] { return addendsFunc(number)}
+    var checksum:Int {return addends.reduce(0, combine: +) }
+    var isValid : Bool { return checksum % 10 == 0}
+    
+    init(_ num:Double){
+    self.number = num
+    }
+    
+    static func create (num:Double)->Double{
+        
+        func createCheckDigit(value:Int) -> Int{
+            var nearestTen = Int(ceil((Double(value) / 10.00)) * 10)
+            return nearestTen - value
+        }
+
+        var zeroCheckDigitNumber = num * 10
+        var luhn = Luhn(zeroCheckDigitNumber)
+        
+        if luhn.isValid {
+            return zeroCheckDigitNumber}
+        
+        return zeroCheckDigitNumber + Double(createCheckDigit(luhn.checksum))
+        
+    }
+    
+    func addendsFunc(num:Double)->[Int]{
+        
+        func oddIndexDoubleMinus9(var input:[Int])->[Int]{
+            input = input.reverse()
+            var tempArray:[Int] = []
+            for (inx, each) in enumerate(input){
+                var tempEach:Int = each
+                if (inx+1) % 2 == 0 {
+                    tempEach *= 2
+                    if tempEach > 10 {
+                        tempEach -= 9
+                    }
+                    tempArray.insert(tempEach, atIndex: 0)
+                } else {
+                    tempArray.insert(tempEach, atIndex: 0)
+                }
+            }
+            return tempArray
+        }
+        
+        func char2Int(input:Character)-> Int{
+            let tempInt = String(input).toInt() ?? -1 // -1 = error
+            return tempInt
+        }
+        
+        
+        
+        let tempString = "\(Int64(num))"
+        
+        return oddIndexDoubleMinus9(Array(tempString).map{char2Int($0)})
+    }
+    
+    
+    
+}
