5e24120cd279dcdb71905819e8b8a24bfb55d9ac
Masters3d <cheyo@masters3d.com>
2015-04-12 19:44:17 -0700

    update sum of multiples
    https://github.com/exercism/exercism.io/issues/2341

--- sum-of-multiples/SumOfMultiplesTest.swift
+++ sum-of-multiples/SumOfMultiplesTest.swift
@@ -3,11 +3,8 @@ import XCTest
 
 class SumOfMultiplesTest:XCTestCase {
     
-    
     func testSumTo1(){
-        let result = SumOfMultiples.toLimit(1)
-        
-        XCTAssertEqual(0, result)
+        XCTAssertEqual(0, SumOfMultiples.toLimit(1))
     }
     
     func testSumTo3(){
@@ -24,20 +21,23 @@ class SumOfMultiplesTest:XCTestCase {
     
     func testConfigurable_7_13_17_to_20(){
         XCTAssertEqual(51,  SumOfMultiples.toLimit(20, inMultiples: [7, 13, 17]))
-
+    }
+    
+    func testConfigurable_4_6_to_15(){
+        XCTAssertEqual(30,  SumOfMultiples.toLimit(15, inMultiples: [4, 6]))
+    }
+    
+    func testConfigurable_5_6_8_to_150(){
+        XCTAssertEqual(4419,  SumOfMultiples.toLimit(150, inMultiples: [5, 6, 8]))
     }
     
     func testConfigurable_43_47_to_10000(){
         XCTAssertEqual(2203160,  SumOfMultiples.toLimit(10000, inMultiples: [43, 47]))
-        
     }
 
-    
     func testConfigurable_0_to_10(){
         XCTAssertEqual(0,  SumOfMultiples.toLimit(10, inMultiples: [0]))
-        
     }
-
 
     func testConfigurable_0_1_to_10(){
         XCTAssertEqual(45,  SumOfMultiples.toLimit(10, inMultiples: [0, 1]))
