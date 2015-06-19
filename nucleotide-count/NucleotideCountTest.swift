7e45b9d56783268fbf6b65dd216176668c299469
Aarti Parikh <aarti@sachmanya.com>
2015-06-19 11:42:53 -0700

    Use enum and struct to model nucleotide and DNA

--- nucleotide-count/NucleotideCountTest.swift
+++ nucleotide-count/NucleotideCountTest.swift
@@ -1,60 +1,60 @@
 import XCTest
 
-class NucleotideCountTest: XCTestCase
+class NucleotideCountTests: XCTestCase
 {
     func testEmptyDNAStringHasNoAdenosine() {
-        let dna = DNA.withStrand("")!
+        let dna = DNA(strand: "")!
         let result = dna.count("A")
         let expected = 0
-        XCTAssertEqual(result!, expected)
+        XCTAssertEqual(result, expected)
     }
     
+    // An initializer defined with init can be made failable by adding a ? or a ! after the init
     func testEmptyDNAStringHasNoNucleotides() {
-        let dna = DNA.withStrand("")!
+        let dna = DNA(strand: "")!
         let results = dna.nucleotideCounts
-        let expected = [ "A": 0, "T" : 0, "C" : 0, "G" : 0 ]
+        let expected:[Nucleobase:Int] = [ .Adenine: 0, .Thymine: 0, .Cytosine: 0 , .Guanine : 0 ]
         XCTAssertEqual(results, expected)
     }
     
     func testRepetitiveCytidineGetsCounted() {
-        let dna = DNA.withStrand("CCCCC")!
-        let result = dna.count("C")!
+        let dna = DNA(strand: "CCCCC")!
+        let result = dna.count("C")
         let expected = 5
         XCTAssertEqual(result, expected)
     }
     
     func testRepetitiveSequenceHasOnlyGuanosine() {
-        let dna = DNA.withStrand("GGGGGGGG")!
+        let dna = DNA(strand: "GGGGGGGG")!
         let results = dna.nucleotideCounts
-        let expected = [ "A": 0, "T" : 0, "C" : 0, "G" : 8 ]
+        let expected:[Nucleobase:Int] = [ .Adenine: 0, .Thymine: 0, .Cytosine: 0 , .Guanine : 8 ]
         XCTAssertEqual(results, expected)
     }
     
     func testCountsByThymidine() {
-        let dna = DNA.withStrand("GGGGGTAACCCGG")!
-        let result = dna.count("T")!
+        let dna = DNA(strand: "GGGGGTAACCCGG")!
+        let result = dna.count("T")
         let expected = 1
         XCTAssertEqual(result, expected)
     }
     
     func testCountsANucleotideOnlyOnce() {
-        let dna = DNA.withStrand("CGATTGGG")!
-        var result = dna.count("T")!
-        result = dna.count("T")!
+        let dna = DNA(strand: "CGATTGGG")!
+        var result = dna.count("T")
+        result = dna.count("T")
         let expected = 2
         XCTAssertEqual(result, expected)
     }
     
     func testValidatesDNA() {
-        let dna = DNA.withStrand("John")
-        XCTAssert(dna == nil)
+        XCTAssert(DNA(strand: "John") == nil )
     }
     
     func testCountsAllNucleotides() {
-        var longStrand = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
-        let dna = DNA.withStrand(longStrand)!
+        let longStrand = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+        let dna = DNA(strand: longStrand)!
         let results = dna.nucleotideCounts
-        let expected = [ "A": 20, "T" : 21, "C" : 12, "G" : 17 ]
+        let expected:[Nucleobase:Int] = [ .Adenine: 20, .Thymine: 21, .Cytosine: 12 , .Guanine : 17 ]
         XCTAssertEqual(results, expected)
     }
 }
