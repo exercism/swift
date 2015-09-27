d31e6632a3f1b4249721d0ff645206baaa3006c3
Rob Hudson <rhudson@weddingwire.com>
2015-09-27 16:48:52 -0400

    Fix mutability warnings

--- meetup/MeetupTest.swift
+++ meetup/MeetupTest.swift
@@ -10,47 +10,47 @@ class  MeetupTest: XCTestCase {
     
     
     func testMonteenthOfMay2013() {
-        var meetUp = Meetup(year: 2013, month: 5)
+        let meetUp = Meetup(year: 2013, month: 5)
         XCTAssertEqual("2013-05-13",  meetUp.day(dayOfWeek.Monday, which: whichOptions.teenth).description)
     }
     
     func testSaturteenthOfFebruary2013() {
-        var meetUp = Meetup(year: 2013, month: 2)
+        let meetUp = Meetup(year: 2013, month: 2)
         XCTAssertEqual ("2013-02-16", meetUp.day(dayOfWeek.Saturday, which: whichOptions.teenth).description)
     }
     
     func testFirstTuesdayOfMay2013() {
-        var meetUp = Meetup(year: 2013, month: 5)
+        let meetUp = Meetup(year: 2013, month: 5)
         XCTAssertEqual("2013-05-07", meetUp.day(dayOfWeek.Tuesday, which: whichOptions.first).description)
     }
     
     func testSecondMondayOfApril2013() {
-        var meetUp = Meetup(year: 2013, month: 4)
+        let meetUp = Meetup(year: 2013, month: 4)
         XCTAssertEqual("2013-04-08", meetUp.day(dayOfWeek.Monday, which: whichOptions.second).description)
     }
     
     func testThirdThursdayOfSeptember2013() {
-        var meetUp = Meetup(year: 2013, month: 9)
+        let meetUp = Meetup(year: 2013, month: 9)
         XCTAssertEqual("2013-09-19", meetUp.day(dayOfWeek.Thursday, which: whichOptions.third).description)
     }
     
     func testFourthSundayOfMarch2013() {
-        var meetUp = Meetup(year: 2013, month: 3)
+        let meetUp = Meetup(year: 2013, month: 3)
         XCTAssertEqual("2013-03-24", meetUp.day(dayOfWeek.Sunday, which: whichOptions.fourth).description)
     }
     
     func testLastThursdayOfOctober2013() {
-        var meetUp = Meetup(year: 2013, month: 10)
+        let meetUp = Meetup(year: 2013, month: 10)
         XCTAssertEqual("2013-10-31", meetUp.day(dayOfWeek.Thursday, which: whichOptions.last).description)
     }
     
     func testLastWednesdayOfFebruary2012() {
-        var meetUp = Meetup(year: 2012, month: 2)
+        let meetUp = Meetup(year: 2012, month: 2)
         XCTAssertEqual("2012-02-29", meetUp.day(dayOfWeek.Wednesday, which: whichOptions.last).description)
     }
     
     func testFirstFridayOfDecember2012() {
-        var meetUp = Meetup(year: 2012, month: 12)
+        let meetUp = Meetup(year: 2012, month: 12)
         XCTAssertEqual("2012-12-07", meetUp.day(dayOfWeek.Friday, which: whichOptions.first).description)
     }
     
