62a7357352805f32327faa6a84e316a7cadc665c
Rob Hudson <rhudson@weddingwire.com>
2016-01-18 16:04:41 -0500

    Clean up

--- robot-simulator/RobotSimulatorTest.swift
+++ robot-simulator/RobotSimulatorTest.swift
@@ -146,37 +146,5 @@ class RobotSimulatorTest: XCTestCase {
         XCTAssertEqual([11, 5], robot3.coordinates)
         XCTAssertEqual(CardinalDirection.North, robot3.bearing)
     }
-    
-//        def test_instruct_robot
-//        skip
-//        robot = Robot.new
-//        simulator.place(robot, x: -2, y: 1, direction: :east)
-//        simulator.evaluate(robot, 'RLAALAL')
-//        assert_equal [0, 2], robot.coordinates
-//        assert_equal :west, robot.bearing
-//        end
-    
-//        def test_instruct_many_robots # rubocop:disable Metrics/MethodLength
-//        skip
-//        robot1 = Robot.new
-//        robot2 = Robot.new
-//        robot3 = Robot.new
-//        simulator.place(robot1, x: 0, y: 0, direction: :north)
-//        simulator.place(robot2, x: 2, y: -7, direction: :east)
-//        simulator.place(robot3, x: 8, y: 4, direction: :south)
-//        simulator.evaluate(robot1, 'LAAARALA')
-//        simulator.evaluate(robot2, 'RRAAAAALA')
-//        simulator.evaluate(robot3, 'LAAARRRALLLL')
-//        
-//        assert_equal [-4, 1], robot1.coordinates
-//        assert_equal :west, robot1.bearing
-//        
-//        assert_equal [-3, -8], robot2.coordinates
-//        assert_equal :south, robot2.bearing
-//        
-//        assert_equal [11, 5], robot3.coordinates
-//        assert_equal :north, robot3.bearing
-//        end
-//        end
-    
+        
 }
