03ec8e212c7b396f7a303a6a40734b429dc4fbda
Rob Hudson <rhudson@weddingwire.com>
2016-01-19 15:48:51 -0500

    Add Queen Attack

---
+++ queen-attack/QueenAttackExample.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+// Apple Swift version 2.1
+
+struct Queens {
+    
+    enum InitError: ErrorType {
+        case SameSpace
+        case IncorrectNumberOfCoordinates
+        case InvalidCoordinates
+    }
+    
+    let white: [Int]
+    let black: [Int]
+    var canAttack: Bool {
+        return onHorizontal || onVertical || onDiagonal
+    }
+    
+    private var onHorizontal: Bool {
+        return white[0] == black[0]
+    }
+    private var onVertical: Bool {
+        return white[1] == black[1]
+    }
+    private var onDiagonal: Bool {
+        return abs(black[1] - black[0]) == abs(white[1] - white[0])
+    }
+    
+    init(white: [Int] = [0, 3], black: [Int] = [7, 3]) throws {
+        guard white.count == 2 && black.count == 2 else {
+            throw InitError.IncorrectNumberOfCoordinates
+        }
+        
+        for number in white + black {
+            guard number >= 0 && number <= 7 else {
+                throw InitError.InvalidCoordinates
+            }
+        }
+        
+        guard white != black else {
+            throw InitError.SameSpace
+        }
+        
+        self.white = white
+        self.black = black
+    }
+
+}
+
+extension Queens: CustomStringConvertible {
+    var description: String {
+        let row = [String](count: 8, repeatedValue: "_")
+        var board = [[String]](count: 8, repeatedValue: row)
+        
+        board[white[0]][white[1]] = "W"
+        board[black[0]][black[1]] = "B"
+        
+        var rows = [String]()
+        
+        for row in board {
+            rows.append(row.joinWithSeparator(" "))
+        }
+        
+        return rows.joinWithSeparator("\n")
+    }
+}
