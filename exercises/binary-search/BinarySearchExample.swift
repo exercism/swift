0e554d3d0b7c1a172ac9e22c444dea25d381a8cf
Rob Hudson <rhudson@weddingwire.com>
2016-01-20 21:59:29 -0500

    Merge pull request #100 from robtimp/binary-search Add Binary Search
    Merge fixes.

---
+++ exercises/binary-search/BinarySearchExample.swift
@@ -0,0 +1,53 @@
+// Foundation not needed
+
+// Apple Swift version 2.1
+
+// Note: Placing this enum inside the BinarySearch struct results in a compiler crash
+enum BinarySearchError: ErrorType {
+    case Unsorted
+}
+
+struct BinarySearch<T: Comparable> {
+    
+    let list: [T]
+    var middle: Int {
+        return list.count / 2
+    }
+    
+    init(_ list: [T]) throws {
+        guard list == list.sort(<) else {
+            throw BinarySearchError.Unsorted
+        }
+        
+        self.list = list
+    }
+    
+    func searchFor(datum: T) -> Int? {
+        let middleItem = list[middle]
+        
+        if middleItem == datum {
+            return middle
+        } else if middleItem > datum {
+            let sublist = Array(list[0...middle])
+            guard sublist != list else {
+                return nil
+            }
+            
+            // try! is safe here, since it's not possible to get here if the data isn't initially sorted
+            return try! BinarySearch(sublist).searchFor(datum)
+        } else {
+            let sublist = Array(list[middle..<list.count])
+            guard sublist != list else {
+                return nil
+            }
+            
+            // try! is safe here, since it's not possible to get here if the data isn't initially sorted
+            if let index = try! BinarySearch(sublist).searchFor(datum) {
+                return index + middle
+            } else {
+                return nil
+            }
+        }
+    }
+    
+}
---
+++ exercises/binary-search/BinarySearchTest.swift
@@ -0,0 +1,53 @@
+import XCTest
+
+// Apple Swift version 2.1
+
+class BinarySearchTest: XCTestCase {
+    
+    func testHasListData() {
+        let binary = try! BinarySearch([1, 3, 4, 6, 8, 9, 11])
+        XCTAssertEqual([1, 3, 4, 6, 8, 9, 11], binary.list)
+    }
+
+    func testThrowsErrorForUnsortedList() {
+        var throwsUnsortedError = false
+        
+        defer {
+            XCTAssertTrue(throwsUnsortedError)
+        }
+        
+        do {
+            let _ = try BinarySearch([2, 1, 4, 3, 6])
+        } catch BinarySearchError.Unsorted {
+            throwsUnsortedError = true
+        } catch {
+            return
+        }
+    }
+
+    func testNilForDataNotInList() {
+        XCTAssertNil(try! BinarySearch([1, 3, 6]).searchFor(2))
+    }
+    
+    func testFindsPositionOfMiddleItem() {
+        let binary = try! BinarySearch([1, 3, 4, 6, 8, 9, 11])
+        XCTAssertEqual(3, binary.middle) 
+    }
+    
+    func testFindsPositionOfSearchData() {
+        let binary = try! BinarySearch([1, 3, 4, 6, 8, 9, 11])
+        XCTAssertEqual(5, binary.searchFor(9))
+    }
+    
+    func testFindsPositionInALargerList() {
+        let binary = try! BinarySearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144])
+        XCTAssertEqual(1, binary.searchFor(3))
+        XCTAssertEqual(7, binary.searchFor(55))
+    }
+    
+    func testFindsCorrectPositionInAListWithAnEvenNumberOfElements() {
+        let binary = try! BinarySearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])
+        XCTAssertEqual(5, binary.searchFor(21))
+        XCTAssertEqual(6, binary.searchFor(34))
+    }
+}
