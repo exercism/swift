749c0ddca4f1672221c2581214620c7796e3a9d5
Rob Hudson <rhudson@weddingwire.com>
2016-01-29 11:11:04 -0500

    Initialize from Hexadecimal object

--- exercises/hexadecimal/HexadecimalExample.swift
+++ exercises/hexadecimal/HexadecimalExample.swift
@@ -2,32 +2,40 @@
 
 // Apple Swift version 2.1
 
-extension Int {
+struct Hexadecimal {
     
-    init?(hex: String) {
-        let hexDigits = [
-            "0" : 0,
-            "1" : 1,
-            "2" : 2,
-            "3" : 3,
-            "4" : 4,
-            "5" : 5,
-            "6" : 6,
-            "7" : 7,
-            "8" : 8,
-            "9" : 9,
-            "a" : 10,
-            "b" : 11,
-            "c" : 12,
-            "d" : 13,
-            "e" : 14,
-            "f" : 15
-        ]
-
+    let hexString: String
+    let intValue: Int?
+    
+    private static let hexDigits = [
+        "0" : 0,
+        "1" : 1,
+        "2" : 2,
+        "3" : 3,
+        "4" : 4,
+        "5" : 5,
+        "6" : 6,
+        "7" : 7,
+        "8" : 8,
+        "9" : 9,
+        "a" : 10,
+        "b" : 11,
+        "c" : 12,
+        "d" : 13,
+        "e" : 14,
+        "f" : 15
+    ]
+    
+    init(_ hexString: String) {
+        self.hexString = hexString
+        self.intValue = Hexadecimal.getIntFromHexString(hexString)
+    }
+    
+    private static func getIntFromHexString(hexString: String) -> Int? {
         var result = 0
         var multiplier = 1
         
-        let digits = hex.characters.map { String($0).lowercaseString }.reverse()
+        let digits = hexString.characters.map { String($0).lowercaseString }.reverse()
         
         for digit in digits {
             guard let intValue = hexDigits[digit] else {
@@ -39,7 +47,18 @@ extension Int {
             multiplier *= 16
         }
         
-        self = result
+        return result
+    }
+}
+
+extension Int {
+    
+    init?(_ hex: Hexadecimal) {
+        guard let intValue = hex.intValue else {
+            return nil
+        }
+        
+        self = intValue
     }
     
 }
--- exercises/hexadecimal/HexadecimalTest.swift
+++ exercises/hexadecimal/HexadecimalTest.swift
@@ -5,43 +5,43 @@ import XCTest
 class HexadecimalTest: XCTestCase {
     
     func testHex1IsDecimal1() {
-        XCTAssertEqual(1, Int(hex: "1"))
+        XCTAssertEqual(1, Int(Hexadecimal("1")))
     }
     
     func testHexCIsDecimal12() {
-        XCTAssertEqual(12, Int(hex: "c"))
+        XCTAssertEqual(12, Int(Hexadecimal("c")))
     }
     
     func testHex10IsDecimal16() {
-        XCTAssertEqual(16, Int(hex: "10"))
+        XCTAssertEqual(16, Int(Hexadecimal("10")))
     }
     
     func testHexAFIsDecimal175() {
-        XCTAssertEqual(175, Int(hex: "af"))
+        XCTAssertEqual(175, Int(Hexadecimal("af")))
     }
     
     func testHex100IsDecimal256() {
-        XCTAssertEqual(256, Int(hex: "100"))
+        XCTAssertEqual(256, Int(Hexadecimal("100")))
     }
     
     func testHex19aceIsDecimal105166() {
-        XCTAssertEqual(105_166, Int(hex: "19ace"))
+        XCTAssertEqual(105_166, Int(Hexadecimal("19ace")))
     }
     
     func testInvalidHexIsNil() {
-        XCTAssertNil(Int(hex: "carrot"))
+        XCTAssertNil(Int(Hexadecimal("carrot")))
     }
     
     func testBlack() {
-        XCTAssertEqual(0, Int(hex: "000000"))
+        XCTAssertEqual(0, Int(Hexadecimal("000000")))
     }
     
     func testWhite() {
-        XCTAssertEqual(16_777_215, Int(hex: "ffffff"))
+        XCTAssertEqual(16_777_215, Int(Hexadecimal("ffffff")))
     }
     
     func testYellow() {
-        XCTAssertEqual(16_776_960, Int(hex: "ffff00"))
+        XCTAssertEqual(16_776_960, Int(Hexadecimal("ffff00")))
     }
     
 }
