0fe1dbbeab4f0145b58e1ec54bea0d3f7af8df7f
Rob Hudson <rhudson@weddingwire.com>
2016-02-02 17:21:48 -0500

    Add House exercise

---
+++ exercises/house/HouseExample.swift
@@ -0,0 +1,37 @@
+// Foundation not needed
+
+// Apple Swift version 2.1
+    
+struct House {
+    
+    private static let pieces = [
+        ["the horse and the hound and the horn", "that belonged to"],
+        ["the farmer sowing his corn", "that kept"],
+        ["the rooster that crowed in the morn", "that woke"],
+        ["the priest all shaven and shorn", "that married"],
+        ["the man all tattered and torn", "that kissed"],
+        ["the maiden all forlorn", "that milked"],
+        ["the cow with the crumpled horn", "that tossed"],
+        ["the dog", "that worried"],
+        ["the cat", "that killed"],
+        ["the rat", "that ate"],
+        ["the malt", "that lay in"],
+        ["the house that Jack built"]
+    ]
+    
+    static func recite() -> String {
+        return (1...pieces.count).map { line($0) }.joinWithSeparator("\n\n")
+    }
+    
+    private static func line(number: Int) -> String {
+        let startIndex = pieces.count - number
+        let endIndex = pieces.count
+        let selectedPieces = pieces[startIndex ..< endIndex]
+        
+        let text = selectedPieces.map { $0.joinWithSeparator("\n") }.joinWithSeparator(" ")
+        
+        return "This is \(text)."
+    }
+    
+}
+
