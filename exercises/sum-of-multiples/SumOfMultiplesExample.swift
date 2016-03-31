b98691d1d2b4218b11d5cad1e65624dff6d5a59e
Rob Hudson <rhudson@weddingwire.com>
2016-03-31 17:46:37 -0400

    Remove default multiples (issue #127)

--- exercises/sum-of-multiples/SumOfMultiplesExample.swift
+++ exercises/sum-of-multiples/SumOfMultiplesExample.swift
@@ -1,32 +1,28 @@
 // Foundation not needed
 
-
-
 struct SumOfMultiples {
     
-static func toLimit (limit:Int,   inMultiples:[Int] = [] ) -> Int{
-    var multiples = inMultiples
-    let arrayLimit = Array(1..<limit)
-    
-    if multiples.count == 0 {
-        multiples = [3, 5]
-    } else if multiples[0] == 0{
-        multiples = Array(inMultiples.dropFirst())
-    }
-    
-    var itemToReturn = 0
-    
-    for each in arrayLimit{
-        var lastAdded = 0
-        for multiple in multiples {
-            if each % multiple == 0 {
-                if lastAdded != each {
-                    itemToReturn += each
-                    lastAdded = each
+    static func toLimit(limit: Int, inMultiples: [Int]) -> Int {
+        var multiples = Set(inMultiples)
+        
+        if let indexOfZero = multiples.indexOf(0) {
+            multiples.removeAtIndex(indexOfZero)
+        }
+        
+        var itemToReturn = 0
+        
+        for each in 1..<limit {
+            var lastAdded = 0
+            for multiple in multiples {
+                if each % multiple == 0 {
+                    if lastAdded != each {
+                        itemToReturn += each
+                        lastAdded = each
+                    }
                 }
             }
         }
+        
+        return itemToReturn
     }
-    return itemToReturn
-}
 }
