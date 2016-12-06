70bd9281a9c04064844b1088a7c4f6dc615b386c
Alwyn <alwynsavio_concessao@thbs.com>
2016-12-06 12:14:11 +0530

    Implemented the solution the beer song exercise along with unit tests in Swift.

---
+++ exercises/beer-song/BeerSongTest.swift
@@ -0,0 +1,29 @@
+//
+//  beerSongTest.swift
+//  xSwift
+//
+//  Created by Alwyn Savio Concessao on 01/12/16.
+//  Copyright © 2016 exercism.io. All rights reserved.
+//
+#if swift(>=3.0)
+import XCTest
+#endif
+
+
+class BeerSongTest: XCTestCase {
+    
+    var beerSong:BeerSong!
+    override func setUp() {
+        
+            
+        beerSong = BeerSong()
+        
+            
+    }
+    
+    func testForGenerationOfSongVerses(){
+        
+        beerSong.generateVersesOfBeerSong()
+    }
+    
+    }
