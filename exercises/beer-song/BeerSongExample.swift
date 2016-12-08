af9fc9e397bd7d603e1bf91bae96f7edcacaa078
Alwyn <alwynsavio_concessao@thbs.com>
2016-12-08 12:42:21 +0530

    Swift lint integrated into project,added swift lint unscript build phase and committing changes made by swift lint autocorrect.

--- exercises/beer-song/BeerSongExample.swift
+++ exercises/beer-song/BeerSongExample.swift
@@ -1,33 +1,32 @@
-class BeerSong{
+class BeerSong {
 
     let numberOfBeerBottles = 99
     func generateVersesOfBeerSong() -> String {
         var verses = ""
         var numberOfBottlesOfBeer = numberOfBeerBottles
         while numberOfBottlesOfBeer > 2 {
-              verses.append("\(numberOfBottlesOfBeer) bottles of beer on the wall, \(numberOfBottlesOfBeer) bottles of beer.\n")
-              numberOfBottlesOfBeer -= 1
-              verses.append("Take one down and pass it around, \(numberOfBottlesOfBeer) bottles of beer on the wall.\n\n")
-         }
+            verses.append("\(numberOfBottlesOfBeer) bottles of beer on the wall, \(numberOfBottlesOfBeer) bottles of beer.\n")
+            numberOfBottlesOfBeer -= 1
+            verses.append("Take one down and pass it around, \(numberOfBottlesOfBeer) bottles of beer on the wall.\n\n")
+        }
         while numberOfBottlesOfBeer <= 2 {
-        switch(numberOfBottlesOfBeer){
+            switch(numberOfBottlesOfBeer) {
             case 2:verses.append("\(numberOfBottlesOfBeer) bottles of beer on the wall, \(numberOfBottlesOfBeer) bottles of beer.\n")
-                   numberOfBottlesOfBeer -= 1
-                   verses.append("Take one down and pass it around, \(numberOfBottlesOfBeer) bottle of beer on the wall.\n\n")
-            
+            numberOfBottlesOfBeer -= 1
+            verses.append("Take one down and pass it around, \(numberOfBottlesOfBeer) bottle of beer on the wall.\n\n")
+
             case 1:verses.append("\(numberOfBottlesOfBeer) bottle of beer on the wall, \(numberOfBottlesOfBeer) bottle of beer.\n")
-                   numberOfBottlesOfBeer -= 1
-                   verses.append("Take one down and pass it around, no more bottles of beer on the wall.\n\n")
-        
+            numberOfBottlesOfBeer -= 1
+            verses.append("Take one down and pass it around, no more bottles of beer on the wall.\n\n")
+
             case 0:verses.append("No more bottles of beer on the wall, no more bottles of beer.\n")
-                   numberOfBottlesOfBeer += numberOfBeerBottles
-                   verses.append("Go to the store and buy some more, \(numberOfBottlesOfBeer) bottles of beer on the wall.")
+            numberOfBottlesOfBeer += numberOfBeerBottles
+            verses.append("Go to the store and buy some more, \(numberOfBottlesOfBeer) bottles of beer on the wall.")
             default:break
             }
-        
+
         }
         return verses
     }
 
 }
-
