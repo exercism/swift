1905508e1e1b047f56f997f772e8cdd7d021705c
Alwyn <alwynsavio_concessao@thbs.com>
2017-01-04 13:09:26 +0530

    Code optimisations on flatten array function,updated unit tests for string and double arrays,removed constructList() function.

--- exercises/flatten-array/FlattenArrayExample.swift
+++ exercises/flatten-array/FlattenArrayExample.swift
@@ -1,36 +1,28 @@
 import Foundation
 
-func constructList(elements: Any?...) -> [Any?] {
-
-    return elements
-}
-
 func flattenArray<T>(_ list: [Any?]) -> [T] {
 
     var flattenedArray = [T]()
 
     func extractArrayElements(array: [Any?]) {
 
-        for element in array {
+        for element in array.flatMap({$0}) {
 
-            if let j = element {
+            let anyObjectArray = element as? [Any?]
 
-                let anyObjectArray = j as? [Any?]
+            if let unwrappedArray = anyObjectArray {
 
-                if let unwrappedArray = anyObjectArray {
-
-                    extractArrayElements(array: unwrappedArray)
-
-                }
-
-                let value = j as? T
-
-                if let i = value {
-
-                    flattenedArray.append(i)
-                }
+                extractArrayElements(array: unwrappedArray)
 
             }
+
+            let value = element as? T
+
+            if let i = value {
+
+                flattenedArray.append(i)
+            }
+
         }
 
     }
