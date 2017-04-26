fcc4d0d7617b43885b98b972008de0c5e586141d
Joe Lozar <josef.lozar@gmail.com>
2017-04-26 17:08:30 -0500

    changed the test to use arrays for the sorted comparision.  Both the result and the expected array need to be equal in order to pass the test (if they are not equal the result array is not in the same order as the expected).

--- exercises/grade-school/Tests/GradeSchoolTests/GradeSchoolTests.swift
+++ exercises/grade-school/Tests/GradeSchoolTests/GradeSchoolTests.swift
@@ -3,68 +3,60 @@ import XCTest
 
 class GradeSchoolTests: XCTestCase {
 
-    // Workaround unit test liminitaiton on Optional Arrays/Sets
-    private func sameCollection<C: Collection>(_ result: C?, _ expected: [String]?) -> Bool where C.Iterator.Element == String {
-        guard let result = result, let expected = expected, similar(result, expected) else { return false }
-        for (index, student) in result.enumerated() {
-            guard index < expected.count && expected.contains(student) else { return false }
-        }
-        return true
-    }
-    private func similar<C: Collection>(_ result: C, _ expected: [String]) -> Bool {
-        return result.count.toIntMax() == IntMax(expected.count)
+    func sameArray(_ result: [String]?, _ expected: [String]? ) -> Bool {
+        guard let result = result, let expected = expected else { return false }
+        return result == expected
     }
 
-    func XCTAssertEqualCollection<C:Collection> (_ collectionS: C?, _ collectionA: [String]? ) where C.Iterator.Element == String {
-        XCTAssert(sameCollection(collectionS, collectionA))
+    func XCTAssertEqualArray (_ result: [String]?, _ expected: [String]? ) {
+        XCTAssertTrue(sameArray(expected, result))
     }
 
     func testAnEmptySchool() {
         let school   = GradeSchool()
         let result   = school.roster
         XCTAssertTrue(result.isEmpty)
-
     }
 
     func testAddStudent() {
-        var school = GradeSchool()
+        let school = GradeSchool()
         school.addStudent("Aimee", grade: 2)
         let result = school.roster
         let expected: Dictionary = [2: ["Aimee"]]
         XCTAssertEqual(Array(result.keys), Array(expected.keys))
-        XCTAssertEqualCollection(result[2], expected[2])
+        XCTAssertEqualArray(result[2], expected[2])
     }
 
     func testAddMoreStudentsInSameClass() {
-        var school = GradeSchool()
+        let school = GradeSchool()
         school.addStudent("Fred", grade: 2)
         school.addStudent("James", grade: 2)
         school.addStudent("Paul", grade: 2)
         let result   = school.roster
         let expected = [2: ["Fred", "James", "Paul"]]
         XCTAssertEqual(Array(result.keys), Array(expected.keys))
-        XCTAssertEqualCollection(result[2], expected[2])
+        XCTAssertEqualArray(result[2], expected[2])
     }
 
     func testAddStudentsToDifferentGrades() {
-        var school = GradeSchool()
+        let school = GradeSchool()
         school.addStudent("Chelsea", grade:3)
         school.addStudent("Logan", grade: 7)
         let result = school.roster
         let expected = [3: ["Chelsea"], 7: ["Logan"]]
         XCTAssertEqual(Array(result.keys).sorted(by: >), Array(expected.keys).sorted(by: >))
 
-        XCTAssertEqualCollection(result[3], expected[3])
+        XCTAssertEqualArray(result[3], expected[3])
     }
 
     func testGetStudentsInAGrade() {
-        var school = GradeSchool()
+        let school = GradeSchool()
         school.addStudent("Franklin", grade: 5)
         school.addStudent("Bradley", grade: 5)
         school.addStudent("Jeff", grade: 1)
         let result   = school.studentsInGrade(5)
         let expected = ["Franklin", "Bradley"]
-        XCTAssertEqualCollection(result, expected)
+        XCTAssertEqual(result, expected)
     }
 
     func testGetStudentsInANonExistantGrade() {
@@ -72,11 +64,11 @@ class GradeSchoolTests: XCTestCase {
         let result = school.studentsInGrade(1)
 
         let expected = [String]()
-        XCTAssertEqualCollection(result, expected)
+        XCTAssertEqual(result, expected)
     }
 
     func testSortSchool() {
-        var school = GradeSchool()
+        let school = GradeSchool()
         school.addStudent("Jennifer", grade: 4)
         school.addStudent("Kareem", grade: 6)
         school.addStudent("Christopher", grade: 4)
@@ -88,11 +80,12 @@ class GradeSchoolTests: XCTestCase {
             4: ["Christopher", "Jennifer"],
             6: ["Kareem"]
         ]
+
         XCTAssertEqual(Array(result.keys).sorted(by: >), Array(expected.keys).sorted(by: >))
 
-        XCTAssertEqualCollection(result[3], expected[3])
-        XCTAssertEqualCollection(result[4], expected[4])
-        XCTAssertEqualCollection(result[6], expected[6])
+        XCTAssertEqualArray(result[3], expected[3])
+        XCTAssertEqualArray(result[4], expected[4])
+        XCTAssertEqualArray(result[6], expected[6])
     }
 
     static var allTests: [(String, (GradeSchoolTests) -> () throws -> Void)] {
