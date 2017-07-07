a45ce82c6b7a9d710620356af66027477ddb6d66
Sam Krishna <sam.krishna@gmail.com>
2017-07-06 17:45:03 -0700

    Fixed a few more linting warnings (#281)
    * Fixed a few more linting warnings
    
    * Adding SwiftLint directives to see if this works
    
    * Created compount where-clause to silence linting warning
    
    * Taking another cut at fixing the custom-set warning
    
    * Using @masters3d code for PokerExample.swift (works under Swift 4.0 Dev Toolchain from 2017-07-05)

--- exercises/secret-handshake/Sources/SecretHandshakeExample.swift
+++ exercises/secret-handshake/Sources/SecretHandshakeExample.swift
@@ -13,7 +13,7 @@ struct SecretHandshake {
 
     private func commandsFunc() -> [String] {
         var commands = [String]()
-        for key in Array(commandValues.keys).sorted( by: < ) where (commandValue & key) != 0{
+        for key in Array(commandValues.keys).sorted( by: < ) where (commandValue & key) != 0 {
             commands.append(commandValues[key]!)
         }
 
