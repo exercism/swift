f14ed2764acacc4d79639c4f5812670d5058e0c5
crea <masataka0504@gmail.com>
2017-08-15 23:49:23 -0700

    applied to the swiftlint

--- exercises/rotational-cipher/Sources/RotationalCipherExample.swift
+++ exercises/rotational-cipher/Sources/RotationalCipherExample.swift
@@ -1,31 +1,30 @@
-
 struct RotationalCipher {
-    
+
     static func rotate(_ target: String, ROT: Int) -> String {
-        
+
         var result = ""
-        
+
         target.unicodeScalars.forEach { unicode in
-            
+
             switch unicode.value {
                 case 65...90: // A to Z
                     var scalar = unicode.value + UInt32(ROT)
                     if scalar > 90 { scalar -= 26 }
                     result.append(Character(UnicodeScalar(scalar)!))
-                    
+
                 case 97...122: // a to z
                     var scalar = unicode.value + UInt32(ROT)
                     if scalar > 122 { scalar -= 26 }
                     result.append(Character(UnicodeScalar(scalar)!))
-                    
+
                 default:
                     result.append(Character(unicode))
             }
-            
+
         }
-        
+
         return result
-        
+
     }
-    
+
 }
