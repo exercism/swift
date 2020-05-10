b7e9509c8f4c755667d7d4d6b9cad88cb7ce54ee
WDN <wneumann@gmail.com>
2020-05-10 11:05:43 -0500

    Replaced XCode only XCTUnwrap with XCTAssertNotNil and a guard let.

--- exercises/nucleotide-count/Tests/NucleotideCountTests/NucleotideCountTests.swift
+++ exercises/nucleotide-count/Tests/NucleotideCountTests/NucleotideCountTests.swift
@@ -2,50 +2,71 @@ import XCTest
 @testable import NucleotideCount
 
 class NucleotideCountTests: XCTestCase {
-    func testEmptyDNAStringHasNoAdenosine() throws {
-        let dna = try XCTUnwrap(DNA(strand: ""), "Empty strands are valid strands and should not return nil")
+    func testEmptyDNAStringHasNoAdenosine() {
+        guard let dna = DNA(strand: "") else {
+            XCTAssert(false, "Empty strands are valid strands and should not return nil")
+            return
+        }
         let result = dna.count("A")
         let expected = 0
         XCTAssertEqual(result, expected)
     }
 
-    func testEmptyDNAStringHasNoNucleotides() throws {
-        let dna = try XCTUnwrap(DNA(strand: ""), "Empty strands are valid strands and should not return nil")
+    func testEmptyDNAStringHasNoNucleotides() {
+        guard let dna = DNA(strand: "") else {
+            XCTAssert(false, "Empty strands are valid strands and should not return nil")
+            return
+        }
         let results = dna.counts()
         let expected = ["T": 0, "A": 0, "C": 0, "G": 0]
         XCTAssertEqual(results, expected)
     }
 
-    func testSingleNoNucleotideStrand() throws {
-        let dna = try XCTUnwrap(DNA(strand: "T"))
+    func testSingleNoNucleotideStrand() {
+        guard let dna = DNA(strand: "T") else {
+            XCTAssert(false, "Valid strands should not return nil")
+            return
+        }
         let results = dna.counts()
         let expected = ["T": 1, "A": 0, "C": 0, "G": 0]
         XCTAssertEqual(results, expected)
     }
 
-    func testRepetitiveCytidineGetsCounted() throws {
-        let dna = try XCTUnwrap(DNA(strand: "CCCCC"))
+    func testRepetitiveCytidineGetsCounted() {
+        guard let dna = DNA(strand: "CCCCC") else {
+            XCTAssert(false, "Valid strands should not return nil")
+            return
+        }
         let result = dna.count("C")
         let expected = 5
         XCTAssertEqual(result, expected)
     }
 
-    func testRepetitiveSequenceHasOnlyGuanosine() throws {
-        let dna = try XCTUnwrap(DNA(strand: "GGGGGGGG"))
+    func testRepetitiveSequenceHasOnlyGuanosine() {
+        guard let dna = DNA(strand: "GGGGGGGG") else {
+            XCTAssert(false, "Valid strands should not return nil")
+            return
+        }
         let results = dna.counts()
         let expected = [ "A": 0, "T": 0, "C": 0, "G": 8 ]
         XCTAssertEqual(results, expected)
     }
 
-    func testCountsByThymidine() throws {
-        let dna = try XCTUnwrap(DNA(strand: "GGGGGTAACCCGG"))
+    func testCountsByThymidine() {
+        guard let dna = DNA(strand: "GGGGGTAACCCGG") else {
+            XCTAssert(false, "Valid strands should not return nil")
+            return
+        }
         let result = dna.count("T")
         let expected = 1
         XCTAssertEqual(result, expected)
     }
 
-    func testCountsANucleotideOnlyOnce() throws {
-        let dna = try XCTUnwrap(DNA(strand: "CGATTGGG"))
+    func testCountsANucleotideOnlyOnce() {
+        guard let dna = DNA(strand: "CGATTGGG") else {
+            XCTAssert(false, "Valid strands should not return nil")
+            return
+        }
         let result = dna.count("T")
         let expected = 2
         XCTAssertEqual(result, expected)
@@ -59,9 +80,12 @@ class NucleotideCountTests: XCTestCase {
         XCTAssertNil(DNA(strand: "CATTAX"), "All characters in a strand need to be valid nucleotides.")
     }
 
-    func testCountsAllNucleotides() throws {
+    func testCountsAllNucleotides() {
         let longStrand = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
-        let dna = try XCTUnwrap(DNA(strand: longStrand))
+        guard let dna = DNA(strand: longStrand) else {
+            XCTAssert(false, "Valid strands should not return nil")
+            return
+        }
         let results = dna.counts()
         let expected = [ "A": 20, "T": 21, "C": 12, "G": 17 ]
         XCTAssertEqual(results, expected)
