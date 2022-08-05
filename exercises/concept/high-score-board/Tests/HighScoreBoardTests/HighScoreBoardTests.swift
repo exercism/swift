56c1dc77c3890a48b64f8f0e09fe5c6f7503caba
Vincent Gunawan <gnwnvincent@gmail.com>
2022-08-05 18:29:06 +0700

    Update HighScoreBoardTests.swift

--- exercises/concept/high-score-board/Tests/HighScoreBoardTests/HighScoreBoardTests.swift
+++ exercises/concept/high-score-board/Tests/HighScoreBoardTests/HighScoreBoardTests.swift
@@ -88,7 +88,7 @@ final class HighScoreBoardTests: XCTestCase {
     addPlayer(&scoreboard, "Amil PAstorius", 99373)
     addPlayer(&scoreboard, "Min-seo Shin")
     updateScore(&scoreboard, "Min-seo Shin", 1999)
-    updateScore(&scoreboard, "Jesse Johnson", 1337)
+    updateScore(&scoreboard, "Jesse Johnson", 2674)
     XCTAssertEqual(
       scoreboard,
       ["Jesse Johnson": 2674, "Amil PAstorius": 99373, "Min-seo Shin": 1999],
