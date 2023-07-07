346f10c8b82b9b7e9e63a27a28efb57cd1d1440b
Carl <meatballsofmeatballs@gmail.com>
2023-07-07 10:10:42 +0200

    Fix naming

--- exercises/concept/wings-quest/Package.swift
+++ exercises/concept/wings-quest/Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "WindQuest",
+    name: "WingsQuest",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "WindQuest",
-            targets: ["WindQuest"]),
+            name: "WingsQuest",
+            targets: ["WingsQuest"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "WindQuest",
+            name: "WingsQuest",
             dependencies: []),
         .testTarget(
-            name: "WindQuestTests",
-            dependencies: ["WindQuest"]),
+            name: "WingsQuestTests",
+            dependencies: ["WingsQuest"]),
     ]
 )
