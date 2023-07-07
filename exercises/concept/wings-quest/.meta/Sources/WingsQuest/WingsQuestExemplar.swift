c4d0f277ed2efc6be4b95b8ff3791eb45e1d7d16
Carl <meatballsofmeatballs@gmail.com>
2023-07-07 10:16:52 +0200

    Fix variable name

--- exercises/concept/wings-quest/.meta/Sources/WingsQuest/WingsQuestExemplar.swift
+++ exercises/concept/wings-quest/.meta/Sources/WingsQuest/WingsQuestExemplar.swift
@@ -1,15 +1,15 @@
-func bonusPoints(powerUpActive : Bool, touchingAnotherBird : Bool) -> Bool {
-    return powerUpActive && touchingAnotherBird
+func bonusPoints(powerUpActive : Bool, touchingEagle : Bool) -> Bool {
+    return powerUpActive && touchingEagle
 }
 
 func score(touchingPowerUp : Bool, touchingSeed : Bool) -> Bool {
     return touchingPowerUp || touchingSeed
 }
 
-func lose(powerUpActive : Bool, touchingAnotherBird : Bool) -> Bool{
-    return !powerUpActive && touchingAnotherBird
+func lose(powerUpActive : Bool, touchingEagle : Bool) -> Bool{
+    return !powerUpActive && touchingEagle
 }
 
-func win(HasPickedUpAllSeeds : Bool, powerUpActive : Bool, touchingAnotherBird : Bool) -> Bool{
-    return HasPickedUpAllSeeds && !lose(powerUpActive: powerUpActive, touchingAnotherBird: touchingAnotherBird)
+func win(HasPickedUpAllSeeds : Bool, powerUpActive : Bool, touchingEagle : Bool) -> Bool{
+    return HasPickedUpAllSeeds && !lose(powerUpActive: powerUpActive, touchingEagle: touchingEagle)
 }
