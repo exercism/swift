1a75a2c367c2e770090c3c0dab4f12fb2ee5726b
Carl <meatballsofmeatballs@gmail.com>
2023-08-21 22:32:06 +0200

    Fixes

--- exercises/practice/grade-school/.meta/Sources/GradeSchool/GradeSchoolExample.swift
+++ exercises/practice/grade-school/.meta/Sources/GradeSchool/GradeSchoolExample.swift
@@ -1,15 +1,20 @@
 struct GradeSchool {
   var rosters = [Int: [String]]()
 
-  mutating func addStudent(_ name: String, grade: Int) {
-    
+  mutating func addStudent(_ name: String, grade: Int) -> Bool {
+    if rosters.values.contains { $0.contains(name) } {
+      return false
+    }
+    rosters[grade, default: []].append(name)
+    return true
   }
 
   func studentsInGrade(_ grade: Int) -> [String] {
-    return rosters[grade] ?? [String]()
+    return (rosters[grade] ?? [String]()).sorted()
   }
 
   func roster() -> [String] {
-    return rosters.keys.sorted().flatMap { rosters[$0] ?? [String]() }
+    print(rosters)
+    return rosters.keys.sorted().flatMap { (rosters[$0] ?? [String]()).sorted() }
   }
 }
--- exercises/practice/grade-school/Sources/GradeSchool/GradeSchool.swift
+++ exercises/practice/grade-school/Sources/GradeSchool/GradeSchool.swift
@@ -1,20 +1,3 @@
-struct GradeSchool {
-  var rosters = [Int: [String]]()
-
-  mutating func addStudent(_ name: String, grade: Int) -> Bool {
-    if rosters.values.contains { $0.contains(name) } {
-      return false
-    }
-    rosters[grade, default: []].append(name)
-    return true
-  }
-
-  func studentsInGrade(_ grade: Int) -> [String] {
-    return (rosters[grade] ?? [String]()).sorted()
-  }
-
-  func roster() -> [String] {
-    print(rosters)
-    return rosters.keys.sorted().flatMap { (rosters[$0] ?? [String]()).sorted() }
-  }
+class GradeSchool {
+  // Write your code for the 'GradeSchool' exercise in this file.
 }
