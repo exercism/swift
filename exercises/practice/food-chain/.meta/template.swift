38d0cbf85612a3d1ff466efd29d57cb3857c2618
meatball <meatballsofmeatballs@gmail.com>
2024-12-20 19:46:37 +0100

    Update Swift tools version to 6.0 and refactor test templates

--- exercises/practice/food-chain/.meta/template.swift
+++ exercises/practice/food-chain/.meta/template.swift
@@ -1,17 +1,19 @@
-import XCTest
+import Testing
+import Foundation
 @testable import {{exercise|camelCase}}
-class {{exercise|camelCase}}Tests: XCTestCase {
-    let runAll = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
 
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
+
+@Suite struct {{exercise|camelCase}}Tests {
     {% for case in cases %}
     {% if forloop.first -%}
-        func test{{case.description |camelCase }}() {
+        @Test("{{case.description}}")
     {% else -%}
-        func test{{case.description |camelCase }}() throws {
-        try XCTSkipIf(true && !runAll) // change true to false to run this test
+        @Test("{{case.description}}", .enabled(if: RUNALL))
     {% endif -%}
+    func test{{case.description |camelCase }}() {
         let expected = "{{case.expected | join:"\n" + ""}}"
-        XCTAssertEqual(expected, FoodChain.song(start: {{case.input.startVerse}}, end: {{case.input.endVerse}}))
+        #expect(expected == FoodChain.song(start: {{case.input.startVerse}}, end: {{case.input.endVerse}}))
     }
     {% endfor -%}
 }
