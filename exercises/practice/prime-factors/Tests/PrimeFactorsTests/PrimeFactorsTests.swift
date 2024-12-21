513243a09d7d2bc9ac63a90ecbdd733dbfe09da9
meatball <meatballsofmeatballs@gmail.com>
2024-12-21 15:19:24 +0100

    Update Swift tools version to 6.0 and improve test templates across exercises

--- exercises/practice/prime-factors/Tests/PrimeFactorsTests/PrimeFactorsTests.swift
+++ exercises/practice/prime-factors/Tests/PrimeFactorsTests/PrimeFactorsTests.swift
@@ -1,66 +1,69 @@
-import XCTest
+import Foundation
+import Testing
 
 @testable import PrimeFactors
 
-class PrimeFactorsTests: XCTestCase {
-  let runAll = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
 
+@Suite struct PrimeFactorsTests {
+
+  @Test("no factors")
   func testNoFactors() {
-    XCTAssertEqual(primeFactors(1), [])
+    #expect(primeFactors(1) == [])
   }
 
-  func testPrimeNumber() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(2), [2])
+  @Test("prime number", .enabled(if: RUNALL))
+  func testPrimeNumber() {
+    #expect(primeFactors(2) == [2])
   }
 
-  func testAnotherPrimeNumber() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(3), [3])
+  @Test("another prime number", .enabled(if: RUNALL))
+  func testAnotherPrimeNumber() {
+    #expect(primeFactors(3) == [3])
   }
 
-  func testSquareOfAPrime() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(9), [3, 3])
+  @Test("square of a prime", .enabled(if: RUNALL))
+  func testSquareOfAPrime() {
+    #expect(primeFactors(9) == [3, 3])
   }
 
-  func testProductOfFirstPrime() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(4), [2, 2])
+  @Test("product of first prime", .enabled(if: RUNALL))
+  func testProductOfFirstPrime() {
+    #expect(primeFactors(4) == [2, 2])
   }
 
-  func testCubeOfAPrime() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(8), [2, 2, 2])
+  @Test("cube of a prime", .enabled(if: RUNALL))
+  func testCubeOfAPrime() {
+    #expect(primeFactors(8) == [2, 2, 2])
   }
 
-  func testProductOfSecondPrime() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(27), [3, 3, 3])
+  @Test("product of second prime", .enabled(if: RUNALL))
+  func testProductOfSecondPrime() {
+    #expect(primeFactors(27) == [3, 3, 3])
   }
 
-  func testProductOfThirdPrime() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(625), [5, 5, 5, 5])
+  @Test("product of third prime", .enabled(if: RUNALL))
+  func testProductOfThirdPrime() {
+    #expect(primeFactors(625) == [5, 5, 5, 5])
   }
 
-  func testProductOfFirstAndSecondPrime() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(6), [2, 3])
+  @Test("product of first and second prime", .enabled(if: RUNALL))
+  func testProductOfFirstAndSecondPrime() {
+    #expect(primeFactors(6) == [2, 3])
   }
 
-  func testProductOfPrimesAndNonPrimes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(12), [2, 2, 3])
+  @Test("product of primes and non-primes", .enabled(if: RUNALL))
+  func testProductOfPrimesAndNonPrimes() {
+    #expect(primeFactors(12) == [2, 2, 3])
   }
 
-  func testProductOfPrimes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(901255), [5, 17, 23, 461])
+  @Test("product of primes", .enabled(if: RUNALL))
+  func testProductOfPrimes() {
+    #expect(primeFactors(901255) == [5, 17, 23, 461])
   }
 
-  func testFactorsIncludeALargePrime() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    XCTAssertEqual(primeFactors(93_819_012_551), [11, 9539, 894119])
+  @Test("factors include a large prime", .enabled(if: RUNALL))
+  func testFactorsIncludeALargePrime() {
+    #expect(primeFactors(93_819_012_551) == [11, 9539, 894119])
   }
 }
