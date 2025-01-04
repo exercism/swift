65daba234a63523846029ca80927cc990b33d191
meatball <meatballsofmeatballs@gmail.com>
2025-01-04 22:03:02 +0100

    Fix name of exercise

--- exercises/concept/chessboard/Package.swift
+++ exercises/concept/chessboard/Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-  name: "ChessBoard",
+  name: "Chessboard",
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
-      name: "ChessBoard",
-      targets: ["ChessBoard"])
+      name: "Chessboard",
+      targets: ["Chessboard"])
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "ChessBoard",
+      name: "Chessboard",
       dependencies: []),
     .testTarget(
-      name: "ChessBoardTests",
-      dependencies: ["ChessBoard"]),
+      name: "ChessboardTests",
+      dependencies: ["Chessboard"]),
   ]
 )
