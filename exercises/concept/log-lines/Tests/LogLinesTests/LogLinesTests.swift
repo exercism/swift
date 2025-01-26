b3b138aa1c57fef275a29fe545f5e441ce331d76
meatball <meatballsofmeatballs@gmail.com>
2025-01-26 15:16:09 +0100

    Port test file to Swift 6

--- exercises/concept/log-lines/Tests/LogLinesTests/LogLinesTests.swift
+++ exercises/concept/log-lines/Tests/LogLinesTests/LogLinesTests.swift
@@ -1,98 +1,100 @@
-import XCTest
+import Foundation
+import Testing
 
 @testable import LogLines
 
-final class LogLinesTests: XCTestCase {
-  let runAll = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
 
+@Suite struct LogLinesTests {
+  @Test("init trace")
   func testInitTrace() {
     let line = "[TRC]: Line 84 - Console.WriteLine('Hello World');"
-    XCTAssertEqual(LogLevel(line), LogLevel.trace)
+    #expect(LogLevel(line) == LogLevel.trace)
   }
 
-  func testInitDebug() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("init debug", .enabled(if: RUNALL))
+  func testInitDebug() {
     let line = "[DBG]: ; expected"
-    XCTAssertEqual(LogLevel(line), LogLevel.debug)
+    #expect(LogLevel(line) == LogLevel.debug)
   }
 
-  func testInitInfo() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("init info", .enabled(if: RUNALL))
+  func testInitInfo() {
     let line = "[INF]: Timezone changed"
-    XCTAssertEqual(LogLevel(line), LogLevel.info)
+    #expect(LogLevel(line) == LogLevel.info)
   }
 
-  func testInitWarning() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("init warning", .enabled(if: RUNALL))
+  func testInitWarning() {
     let line = "[WRN]: Timezone not set"
-    XCTAssertEqual(LogLevel(line), LogLevel.warning)
+    #expect(LogLevel(line) == LogLevel.warning)
   }
 
-  func testInitError() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("init error", .enabled(if: RUNALL))
+  func testInitError() {
     let line = "[ERR]: Disk full"
-    XCTAssertEqual(LogLevel(line), LogLevel.error)
+    #expect(LogLevel(line) == LogLevel.error)
   }
 
-  func testInitFatal() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("init fatal", .enabled(if: RUNALL))
+  func testInitFatal() {
     let line = "[FTL]: Not enough memory"
-    XCTAssertEqual(LogLevel(line), LogLevel.fatal)
+    #expect(LogLevel(line) == LogLevel.fatal)
   }
 
-  func testInitUnknownEmpty() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("init unknown empty", .enabled(if: RUNALL))
+  func testInitUnknownEmpty() {
     let line = "Something terrible has happened!"
-    XCTAssertEqual(LogLevel(line), LogLevel.unknown)
+    #expect(LogLevel(line) == LogLevel.unknown)
   }
 
-  func testInitUnknownNonStandard() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("init unknown non-standard", .enabled(if: RUNALL))
+  func testInitUnknownNonStandard() {
     let line = "[XYZ]: Gibberish message.. beep boop.."
-    XCTAssertEqual(LogLevel(line), LogLevel.unknown)
+    #expect(LogLevel(line) == LogLevel.unknown)
   }
 
-  func testShortTrace() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("short trace", .enabled(if: RUNALL))
+  func testShortTrace() {
     let message = "Line 13 - int myNum = 42;"
-    XCTAssertEqual(LogLevel.trace.shortFormat(message: message), "0:Line 13 - int myNum = 42;")
+    #expect(LogLevel.trace.shortFormat(message: message) == "0:Line 13 - int myNum = 42;")
   }
 
-  func testShortDebug() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("short debug", .enabled(if: RUNALL))
+  func testShortDebug() {
     let message = "The name 'LogLevel' does not exist in the current context"
-    XCTAssertEqual(
-      LogLevel.debug.shortFormat(message: message),
-      "1:The name 'LogLevel' does not exist in the current context")
+    #expect(
+      LogLevel.debug.shortFormat(message: message)
+        == "1:The name 'LogLevel' does not exist in the current context")
   }
 
-  func testShortInfo() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("short info", .enabled(if: RUNALL))
+  func testShortInfo() {
     let message = "File moved"
-    XCTAssertEqual(LogLevel.info.shortFormat(message: message), "4:File moved")
+    #expect(LogLevel.info.shortFormat(message: message) == "4:File moved")
   }
 
-  func testShortWarning() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("short warning", .enabled(if: RUNALL))
+  func testShortWarning() {
     let message = "Unsafe password"
-    XCTAssertEqual(LogLevel.warning.shortFormat(message: message), "5:Unsafe password")
+    #expect(LogLevel.warning.shortFormat(message: message) == "5:Unsafe password")
   }
 
-  func testShortError() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("short error", .enabled(if: RUNALL))
+  func testShortError() {
     let message = "Stack overflow"
-    XCTAssertEqual(LogLevel.error.shortFormat(message: message), "6:Stack overflow")
+    #expect(LogLevel.error.shortFormat(message: message) == "6:Stack overflow")
   }
 
-  func testShortFatal() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("short fatal", .enabled(if: RUNALL))
+  func testShortFatal() {
     let message = "Dumping all files"
-    XCTAssertEqual(LogLevel.fatal.shortFormat(message: message), "7:Dumping all files")
+    #expect(LogLevel.fatal.shortFormat(message: message) == "7:Dumping all files")
   }
 
-  func testShortUnknownEmpty() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+  @Test("short unknown empty", .enabled(if: RUNALL))
+  func testShortUnknownEmpty() {
     let message = "Wha happon?"
-    XCTAssertEqual(LogLevel.unknown.shortFormat(message: message), "42:Wha happon?")
+    #expect(LogLevel.unknown.shortFormat(message: message) == "42:Wha happon?")
   }
 }
