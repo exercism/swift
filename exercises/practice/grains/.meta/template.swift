16212011cab80cc88a0b8de6f47ee48477bdee89
meatball <69751659+meatball133@users.noreply.github.com>
2025-06-02 10:56:23 +0200

    [Swift 6]: Update Exercises batch 7 (#791)
    * Update Swift tools version to 6.0 and refactor tests to use new syntax
    
    * Re generate test file

--- exercises/practice/grains/.meta/template.swift
+++ exercises/practice/grains/.meta/template.swift
@@ -1,35 +1,37 @@
-import XCTest
+import Testing
+import Foundation
 @testable import {{exercise|camelCase}}
-class {{exercise|camelCase}}Tests: XCTestCase {
-    let runAll = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
 
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
+
+@Suite struct {{exercise|camelCase}}Tests {
     {% outer: for case in cases %}
         {% ifnot case.expected %}
             {%- for subCases in case.cases %}
             {%- if forloop.outer.first and forloop.first %}
-                func test{{subCases.description |camelCase }}() {
+                @Test("{{subCases.description}}")
             {%- else %}
-                func test{{subCases.description |camelCase }}() throws {
-                try XCTSkipIf(true && !runAll) // change true to false to run this test
+                @Test("{{subCases.description}}", .enabled(if: RUNALL))
             {%- endif %}
+                func test{{subCases.description |camelCase }}() {
             {%- ifnot subCases.expected.error %}
-            XCTAssertEqual(try! Grains.square({{subCases.input.square}}), {{subCases.expected}})
+            #expect(try! Grains.square({{subCases.input.square}}) == {{subCases.expected}})
             {%- else %}
-            XCTAssertThrowsError(try Grains.square({{subCases.input.square}})) { error in
+            #expect(throws:
                 {%- if subCases.input.square < 1 %}
-                XCTAssertEqual(error as? GrainsError, GrainsError.inputTooLow)
+                GrainsError.inputTooLow
                 {%- elif subCases.input.square > 64 %}
-                XCTAssertEqual(error as? GrainsError, GrainsError.inputTooHigh)
+                GrainsError.inputTooHigh
                 {%- endif %}
-            }
+                ){try Grains.square({{subCases.input.square}})}
             {%- endif -%}
 
         }
         {% endfor -%}
         {%- else %}
-        func test{{subCases.description |camelCase }}{{ forloop.outer.counter }}() throws {
-        try XCTSkipIf(true && !runAll) // change true to false to run this test
-        XCTAssertEqual(try! Grains.total, {{case.expected}})
+        @Test("{{subCases.description}}", .enabled(if: RUNALL))
+        func test{{subCases.description |camelCase }}{{ forloop.outer.counter }}() {
+        #expect(try! Grains.total == {{case.expected}})
         }
         {%- endif %}
     {% endfor -%}
