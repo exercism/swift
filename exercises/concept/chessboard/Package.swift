2b4099536c4ffce4d0eb16560262cc983c0fb130
meatball <69751659+meatball133@users.noreply.github.com>
2025-06-02 12:12:39 +0200

    [Swift 6]: Add ranges concept (#823)
    * Add range concept and exercise
    
    * Fix name of exercise
    
    * Fix name again
    
    * Fix name in test file
    
    * Add hints file
    
    * format config file
    
    * Update concepts/ranges/about.md
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>
    
    * Update concepts/ranges/about.md
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>
    
    * Update concepts/ranges/about.md
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>
    
    * Update exercises/concept/chessboard/.docs/instructions.md
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>
    
    * Update exercises/concept/chessboard/.docs/instructions.md
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>
    
    * Update exercises/concept/chessboard/.docs/instructions.md
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>
    
    * Make columns line up
    
    * Various fixes based on feedback
    
    ---------
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>

---
+++ exercises/concept/chessboard/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Chessboard",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "Chessboard",
+      targets: ["Chessboard"])
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "Chessboard",
+      dependencies: []),
+    .testTarget(
+      name: "ChessboardTests",
+      dependencies: ["Chessboard"]),
+  ]
+)
---
+++ exercises/concept/chessboard/Tests/ChessboardTests/ChessboardTests.swift
@@ -0,0 +1,87 @@
+import Testing
+import Foundation
+
+@testable import Chessboard
+
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "true"]) ?? false
+
+@Suite struct ChessboardTests {
+  func generateBoard() -> [String] {
+    var board = [String]()
+    let files2 = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    for rank in 1...8 {
+      for file in files2 {
+        board.append("\(rank)\(file)")
+      }
+    }
+    return board
+  }
+
+  @Test("that there is a 8 correct ranks") 
+  func testRanks() {
+    let expected = 1...8
+    #expect(ranks == expected)
+  }
+
+  @Test("that there is a 8 correct files", .enabled(if: RUNALL))
+  func testFiles() {
+    let expected = "A"..."H"
+    #expect(files == expected)
+  }
+
+  @Test("that it returns true when given a valid square", .enabled(if: RUNALL))
+  func testValidSquare() {
+    let rank = 1
+    let file = "A"
+    #expect(isVaildSquare(rank: rank, file: file))
+  }
+
+  @Test("that it returns true for another valid square", .enabled(if: RUNALL))
+  func testAnotherValidSquare() {
+    let rank = 8
+    let file = "H"
+    #expect(isVaildSquare(rank: rank, file: file))
+  }
+
+  @Test("that it returns false when rank is out of range", .enabled(if: RUNALL))
+  func testRankOutOfRange() {
+    let rank = 9
+    let file = "B"
+    #expect(!isVaildSquare(rank: rank, file: file))
+  }
+
+  @Test("that it returns false when file is out of range", .enabled(if: RUNALL))
+  func testFileOutOfRange() {
+    let rank = 1
+    let file = "I"
+    #expect(!isVaildSquare(rank: rank, file: file))
+  }
+
+  @Test("that it returns false when rank is less than one", .enabled(if: RUNALL))
+  func testRankLessThanOne() {
+    let rank = 0
+    let file = "A"
+    #expect(!isVaildSquare(rank: rank, file: file))
+  }
+
+  @Test("that is returns correct first row", .enabled(if: RUNALL))
+  func testFirstRow() {
+    let board = generateBoard()
+    let expected = ["1A", "1B", "1C", "1D", "1E", "1F", "1G", "1H"]
+    #expect(getRow(board, rank: 1) == expected)
+  }
+
+  @Test("that is returns correct last row", .enabled(if: RUNALL))
+  func testLastRow() {
+    let board = generateBoard()
+    let expected = ["8A", "8B", "8C", "8D", "8E", "8F", "8G", "8H"]
+    #expect(getRow(board, rank: 8) == expected)
+  }
+
+  @Test("that is returns correct middle row", .enabled(if: RUNALL))
+  func testMiddleRow() {
+    let board = generateBoard()
+    let expected = ["4A", "4B", "4C", "4D", "4E", "4F", "4G", "4H"]
+    #expect(getRow(board, rank: 4) == expected)
+  }
+}
