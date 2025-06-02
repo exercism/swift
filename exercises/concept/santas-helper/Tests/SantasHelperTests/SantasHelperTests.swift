39793d066d043d8b6c4932389859a6502aac4aef
meatball <69751659+meatball133@users.noreply.github.com>
2025-06-02 12:53:12 +0200

    [Swift 6]: Reworked tuples and its exercise (#831)
    * Update Santa's Helper exercise with new functions and Swift 6.0 support
    
    * Fixes based on feedback

--- exercises/concept/santas-helper/Tests/SantasHelperTests/SantasHelperTests.swift
+++ exercises/concept/santas-helper/Tests/SantasHelperTests/SantasHelperTests.swift
@@ -1,50 +1,105 @@
-import XCTest
+import Testing
+import Foundation
 
 @testable import SantasHelper
 
-final class SantasHelperTests: XCTestCase {
-  private let runAll =
-    Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
-  private let aboutEqual = { abs($0 - $1) < 0.001 }
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "true"]) ?? false
 
-  func testCartesianToPolar() {
-    let coordinate = (x: 11.713, y: 5.6405)
-    let (actualR, actualPhi) = cartesianToPolar(coordinate)
-    XCTAssertTrue(aboutEqual(actualR, 13.0) && aboutEqual(actualPhi, 0.4488))
+@Suite struct SantasHelperTests {
+  func compareItems(_ items: [(name: String, amount: Int)], _ expected: [(name: String, amount: Int)]) -> Bool {
+    if items.count != expected.count {
+      return false
+    }
+    for i in 0..<items.count {
+      if items[i].name != expected[i].name || items[i].amount != expected[i].amount {
+        return false
+      }
+    }
+    return true
   }
 
-  func testCartesianToPolarQ3() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    let coordinate = (x: -4.7292, y: -2.4096)
-    let (actualR, actualPhi) = cartesianToPolar(coordinate)
-    XCTAssertTrue(aboutEqual(actualR, 5.3077) && aboutEqual(actualPhi, -2.6704))
+  func compareItemsWithCategory(_ items: [(name: String, amount: Int, category: String)], _ expected: [(name: String, amount: Int, category: String)]) -> Bool {
+    if items.count != expected.count {
+      return false
+    }
+    for i in 0..<items.count {
+      if items[i].name != expected[i].name || items[i].amount != expected[i].amount || items[i].category != expected[i].category {
+        return false
+      }
+    }
+    return true
   }
 
-  func testCombineRecords() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
-    let id = Int.random(in: 0..<1_000_000)
-    let toy =
-      ["Chemistry set", "Race car", "Box kite", "Building brick set", "Talking doll"]
-      .randomElement()!
-    let productLead =
-      ["Binkles", "Snowii", "Jinkies", "Pippi", "Zippit", "Xandi", "Alf"]
-      .randomElement()!
-    let children =
-      [
-        "Inés", "Maxime", "Bandile", "Shaurya", "Екатерина", "Anika", "Yael",
-        "Dimitrios", "Евгений", "Iminathi", "Valeria", "Marlon", "Mitsuki",
-        "Bjarni", "Venla", "Anton", "Beatriz", "Joo-won", "Zahra", "Liam", "Parker",
-      ]
-    let numberOfRecipients = Int.random(in: 0..<children.count)
-    let recipients = Array(children.shuffled().prefix(numberOfRecipients))
-    let actual =
-      combineRecords(
-        production: (toy: toy, id: id, productLead: productLead),
-        gifts: (id, recipients)
-      )
-    XCTAssertTrue(
-      actual.id == id && actual.toy == toy && actual.productLead == productLead
-        && actual.recipients == recipients
-    )
+
+  @Test("Can get the name of a toy")
+  func testGetName() {
+    let toy = (name: "Doll", amount: 5)
+    #expect(getName(toy) == "Doll")
+  }
+
+  @Test("Can get the name of another toy", .enabled(if: RUNALL))
+  func testGetName2() {
+    let toy = (name: "Train", amount: 3)
+    #expect(getName(toy) == "Train")
+  }
+
+  @Test("Can create a toy", .enabled(if: RUNALL))
+  func testCreateToy() {
+    let toy = createToy(name: "Doll", amount: 5)
+    #expect(toy == (name: "Doll", amount: 5))
+  }
+
+  @Test("Should allow toys to have zero amount", .enabled(if: RUNALL))
+  func testCreateToyWithZeroAmount() {
+    let toy = createToy(name: "Doll", amount: 0)
+    #expect(toy == (name: "Doll", amount: 0))
+  }
+
+  @Test("Can update the quantity of a toy", .enabled(if: RUNALL))
+  func testUpdateQuantity() {
+    let items = [(name: "Doll", amount: 5), (name: "Train", amount: 3)]
+    let updatedItems = updateQuantity(items, toy: "Doll", amount: 10)
+    let expected = [(name: "Doll", amount: 10), (name: "Train", amount: 3)]
+    #expect(compareItems(updatedItems, expected))
+  }
+
+  @Test("Can update the quantity of another toy", .enabled(if: RUNALL))
+  func testUpdateQuantity2() {
+    let items = [(name: "Doll", amount: 5), (name: "Train", amount: 3)]
+    let updatedItems = updateQuantity(items, toy: "Train", amount: 5)
+    let expected = [(name: "Doll", amount: 5), (name: "Train", amount: 5)]
+    #expect(compareItems(updatedItems, expected))
+  }
+
+  @Test("If the toy is not found, the list should remain the same", .enabled(if: RUNALL))
+  func testUpdateQuantityNotFound() {
+    let items = [(name: "Doll", amount: 5), (name: "Train", amount: 3)]
+    let updatedItems = updateQuantity(items, toy: "Car", amount: 5)
+    let expected = [(name: "Doll", amount: 5), (name: "Train", amount: 3)]
+    #expect(compareItems(updatedItems, expected))
+  }
+
+  @Test("Can add a category to a toy", .enabled(if: RUNALL))
+  func testAddCategory() {
+    let items = [(name: "Car", amount: 5), (name: "Train", amount: 3)]
+    let updatedItems = addCategory(items, category: "Vehicle")
+    let expected = [(name: "Car", amount: 5, "Vehicle"), (name: "Train", amount: 3, "Vehicle")]
+    #expect(compareItemsWithCategory(updatedItems, expected))
+  }
+
+  @Test("Can add a category to another toy", .enabled(if: RUNALL))
+  func testAddCategory2() {
+    let items = [(name: "Doll", amount: 5), (name: "Doll house", amount: 3)]
+    let updatedItems = addCategory(items, category: "Doll")
+    let expected = [(name: "Doll", amount: 5, "Doll"), (name: "Doll house", amount: 3, "Doll")]
+    #expect(compareItemsWithCategory(updatedItems, expected))
+  }
+
+  @Test("If there are no toys it should do nothing", .enabled(if: RUNALL))
+  func testAddCategoryNotFound() {
+    let items: [(name: String, amount: Int)] = []
+    let updatedItems = addCategory(items, category: "Doll")
+    let expected: [(name: String, amount: Int, category: String)] = []
+    #expect(compareItemsWithCategory(updatedItems, expected))
   }
 }
