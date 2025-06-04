678fda42e6e1f7f7c30876f51667241e17789c4d
Seyed Mojtaba Hosseini Zeidabadi <chenzook+github@gmail.com>
2025-06-04 12:00:42 +0330

    refactor: remove unnecessary deprecated function

--- exercises/concept/chessboard/Sources/Chessboard/Chessboard.swift
+++ exercises/concept/chessboard/Sources/Chessboard/Chessboard.swift
@@ -2,11 +2,6 @@
 // TODO: define the 'files' constant
 
 
-@available(*, deprecated, renamed: "isValidSquare(rank:file:)")
-func isVaildSquare(rank: Int, file: String) -> Bool {
-    return isValidSquare(rank: rank, file: file)
-}
-
 func isValidSquare(rank: Int, file: String) -> Bool {
   fatalError("Please implement the isValidSquare(rank:file:) function")
 }
