eaaf5b5d6dadf8a7c2055fa550804e286aea7174
Seyed Mojtaba Hosseini Zeidabadi <chenzook+github@gmail.com>
2025-06-04 11:55:50 +0330

    docs: fix typo in the error message

--- exercises/concept/chessboard/Sources/Chessboard/Chessboard.swift
+++ exercises/concept/chessboard/Sources/Chessboard/Chessboard.swift
@@ -3,9 +3,9 @@
 
 
 func isVaildSquare(rank: Int, file: String) -> Bool {
-  fatalError("Please implement the isVaildSquare(rank:file:) function")
+  fatalError("Please implement the isValidSquare(rank:file:) function")
 }
 
 func getRow(_ board : [String], rank: Int) -> [String] {
-  fatalError("Please implement the isVaildSquare(_:rank:) function")
+  fatalError("Please implement the isValidSquare(_:rank:) function")
 }
