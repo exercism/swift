00bfb559ac8c9483d812fa1ade76160410fa7ce0
Seyed Mojtaba Hosseini Zeidabadi <chenzook+github@gmail.com>
2025-06-04 11:58:31 +0330

    fix: correct the function name and add a backward compatible fallback

--- exercises/concept/chessboard/Sources/Chessboard/Chessboard.swift
+++ exercises/concept/chessboard/Sources/Chessboard/Chessboard.swift
@@ -2,7 +2,12 @@
 // TODO: define the 'files' constant
 
 
+@available(*, deprecated, renamed: "isValidSquare(rank:file:)")
 func isVaildSquare(rank: Int, file: String) -> Bool {
+    return isValidSquare(rank: rank, file: file)
+}
+
+func isValidSquare(rank: Int, file: String) -> Bool {
   fatalError("Please implement the isValidSquare(rank:file:) function")
 }
 
