7c21e9a2f1f95f284f3367a5d4c4dc3751ece26c
meatball <69751659+meatball133@users.noreply.github.com>
2025-06-06 16:35:21 +0000

    Revert freelancer solution

--- exercises/concept/freelancer-rates/.meta/Sources/FreelancerRates/FreelancerRatesExemplar.swift
+++ exercises/concept/freelancer-rates/.meta/Sources/FreelancerRates/FreelancerRatesExemplar.swift
@@ -1,18 +1,11 @@
-let hoursPerDay = 8
-let daysPerMonth = 22
-
 func dailyRateFrom(hourlyRate: Int) -> Double {
-  return Double(hourlyRate * hoursPerDay)
+  fatalError("Please implement the dailyRateFrom(hourlyRate:) function")
 }
 
 func monthlyRateFrom(hourlyRate: Int, withDiscount discount: Double) -> Double {
-  let monthly = Double(hourlyRate * hoursPerDay * daysPerMonth) * (1.0 - (discount / 100))
-  return monthly.rounded()
+  fatalError("Please implement the monthlyRateFrom(hourlyRate:withDiscount:) function")
 }
 
 func workdaysIn(budget: Double, hourlyRate: Int, withDiscount discount: Double) -> Double {
-  let fullMonthlyRate = budget / (1.0 - (discount / 100))
-  let hours = fullMonthlyRate / Double(hourlyRate)
-  let days = hours / Double(hoursPerDay)
-  return days.rounded(.down)
+  fatalError("Please implement the workdaysIn(budget:hourlyRate:withDiscount:) function")
 }
