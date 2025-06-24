d7639b8c58d01f388635abfa00f2bac640a1b7c4
Sencudra <vlad.tarasevich27@gmail.com>
2025-06-24 16:33:15 +0300

    [no important files changed] Rewrite template to Swift Testing (#859)

--- exercises/practice/queen-attack/.meta/template.swift
+++ exercises/practice/queen-attack/.meta/template.swift
@@ -1,38 +1,45 @@
-import XCTest
-@testable import {{exercise|camelCase}}
-class {{exercise|camelCase}}Tests: XCTestCase {
-    let runAll = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
+import Testing
+import Foundation
+@testable import {{exercise | camelCase}}
 
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
+
+@Suite struct {{exercise | camelCase}}Tests {
     {% outer: for case in cases %}
         {%- for subCases in case.cases %}
         {%- if forloop.outer.first and forloop.first %}
-            func test{{subCases.description |camelCase }}() {
+            @Test("{{subCases.description}}")
         {%- else %}
-            func test{{subCases.description |camelCase }}() throws {
-            try XCTSkipIf(true && !runAll) // change true to false to run this test
+            @Test("{{subCases.description}}", .enabled(if: RUNALL))
         {%- endif %}
+            func test{{subCases.description | camelCase}}() throws {
         {%- if subCases.property == "create" %}
             {%- if subCases.expected.error %}
-                XCTAssertThrowsError(try Queen(row: {{subCases.input.queen.position.row}}, column: {{subCases.input.queen.position.column}})) { error in
-                    {%- if subCases.expected.error | contains:"row"  %}
-                    XCTAssertEqual(error as? QueenError, .inValidRow)
-                    {%- else %}
-                    XCTAssertEqual(error as? QueenError, .inValidColumn)
-                    {%- endif %}
+                {%- if subCases.expected.error | contains: "row" %}
+                #expect(throws: QueenError.inValidRow)
+                {%- else %}
+                #expect(throws: QueenError.inValidColumn)
+                {%- endif %}
+                {
+                    try Queen(row: {{subCases.input.queen.position.row}}, column: {{subCases.input.queen.position.column}})
                 }
             {%- else %}
-                let queen = try! Queen(row: {{subCases.input.queen.position.row}}, column: {{subCases.input.queen.position.column}})
-                XCTAssertEqual(queen.row, {{subCases.input.queen.position.row}})
-                XCTAssertEqual(queen.column, {{subCases.input.queen.position.column}})
+                #expect(throws: Never.self) {
+                    let queen = try Queen(row: {{subCases.input.queen.position.row}}, column: {{subCases.input.queen.position.column}})
+                    #expect(queen.row == {{subCases.input.queen.position.row}})
+                    #expect(queen.column == {{subCases.input.queen.position.column}})
+                }
             {%- endif %}
         {%- else %}
-            let queen = try! Queen(row: {{subCases.input.white_queen.position.row}}, column: {{subCases.input.white_queen.position.column}})
-            let otherQueen = try! Queen(row: {{subCases.input.black_queen.position.row}}, column: {{subCases.input.black_queen.position.column}})
+            #expect(throws: Never.self) {
+                let queen = try Queen(row: {{subCases.input.white_queen.position.row}}, column: {{subCases.input.white_queen.position.column}})
+                let otherQueen = try Queen(row: {{subCases.input.black_queen.position.row}}, column: {{subCases.input.black_queen.position.column}})
             {%- if subCases.expected %}
-                XCTAssertTrue(queen.canAttack(other: otherQueen))
+                #expect(queen.canAttack(other: otherQueen))
             {%- else %}
-                XCTAssertFalse(queen.canAttack(other: otherQueen))
+                #expect(!queen.canAttack(other: otherQueen))
             {%- endif %}
+            }
         {%- endif %}
         }
         {% endfor -%}
