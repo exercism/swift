4e2558d6c8c7771b329fa549f1543792c2277eb2
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-07-03 20:40:08 -0700

    Add `flower-field`, deprecating `minesweeper`

---
+++ exercises/practice/flower-field/.meta/template.swift
@@ -0,0 +1,25 @@
+import Testing
+import Foundation
+@testable import {{exercise|camelCase}}
+
+let RUNALL = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
+
+@Suite struct {{exercise|camelCase}}Tests {
+    {% for case in cases %}
+    {% if forloop.first -%}
+        @Test("{{case.description}}")
+    {% else -%}
+        @Test("{{case.description}}", .enabled(if: RUNALL))
+    {% endif -%}
+    func test{{case.description |camelCase }}() {
+        {% ifnot case.input.garden -%}
+        let input = [String]()
+        let output = [String]()
+        {% else -%}
+        let input = {{case.input.garden | toStringArray}}
+        let output = {{case.expected | toStringArray}}
+        {% endif -%}
+        #expect(Board(input).transform() == output)
+    }
+    {% endfor -%}
+}
---
+++ exercises/practice/flower-field/Sources/FlowerField/FlowerField.swift
@@ -0,0 +1,3 @@
+class Board {
+  // Write your code for the 'Flower Field' exercise in this file.
+}
