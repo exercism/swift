22970e137474db644beaad7fd58b9bc5f8554a0c
Sencudra <Sencudra@users.noreply.github.com>
2025-11-26 17:56:45 +0300

    Add exercise

---
+++ exercises/practice/dnd-character/Sources/DndCharacter/DndCharacter.swift
@@ -0,0 +1,13 @@
+func modifier(_ score: Int) -> Int {
+    // Write your code for the 'DndCharacter' exercise here.
+    return 0
+}
+
+func ability() -> Int {
+    // Write your code for the 'DndCharacter' exercise here.
+    return 0
+}
+
+struct DndCharacter {
+  // Write your code for the 'DndCharacter' exercise here.
+}
